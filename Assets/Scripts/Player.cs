ad9c7e79b1c810f8395a09e5f746888f2cca09b3
MidnightZoomies <mnzoomies@gmail.com>
2021-05-04 12:32:59 -0700

    Added boundaries to player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 15f;
+    [SerializeField] float padding = 0.5f;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
     
     /*[SerializeField] float screenWidth = 10;
     [SerializeField] float minX;
@@ -20,12 +26,23 @@ public class Player : MonoBehaviour
     void Start()
     {
         //rb = GetComponent<Rigidbody2D>();
+
+        SetUpMoveBoundaries();
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0,0,0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1,0,0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 
     private void Move()
@@ -42,10 +59,9 @@ public class Player : MonoBehaviour
         */
 
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        var newXPos = transform.position.x + deltaX;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newYPos = transform.position.y + deltaY;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         transform.position = new Vector2(newXPos, newYPos);
-
     }
 }
