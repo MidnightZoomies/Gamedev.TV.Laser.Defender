02c9f2b9ab5e01cbd1509184395d5502c472addb
MidnightZoomies <mnzoomies@gmail.com>
2021-05-04 11:21:40 -0700

    Reverted back to lecture norms - Commented out mouse movement, removed rigidbody/collider, remove walls.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //[SerializeField] float moveSpeed = 15f;
+    [SerializeField] float moveSpeed = 15f;
     
     /*[SerializeField] float screenWidth = 10;
     [SerializeField] float minX;
     [SerializeField] float maxX;
-    */
 
     private Vector3 mousePosition;
     private Rigidbody2D rb;
     private Vector2 direction;
     [SerializeField] private float moveSpeed = 100f;
+    */
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        //rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -30,22 +30,22 @@ public class Player : MonoBehaviour
 
     private void Move()
     {
-        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        /*mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         direction = (mousePosition - transform.position).normalized;
-        rb.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
+        rb.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);*/
 
-        /*float mousePos = Input.mousePosition.x / Screen.width * screenWidth; //the current position of the mouse in relation to the screenwidth
+        /* Cant seem to get this to work right
+        float mousePos = Input.mousePosition.x / Screen.width * screenWidth; //the current position of the mouse in relation to the screenwidth
         Vector2 playerPos = new Vector2(transform.position.x, transform.position.y); //creating vector2 with current object location
         playerPos.x = Mathf.Clamp(mousePos, minX, maxX); //prevents paddle from going off the screen on the x-axis;
         transform.position = playerPos; //changing transform with vector2
         */
 
-        /* var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var newXPos = transform.position.x + deltaX;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         var newYPos = transform.position.y + deltaY;
         transform.position = new Vector2(newXPos, newYPos);
-        */
 
     }
 }
