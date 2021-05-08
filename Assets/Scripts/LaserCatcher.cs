e84420e57c55bde248021638d50dc299039a6581
MidnightZoomies <mnzoomies@gmail.com>
2021-05-08 16:18:33 -0700

    Added Enemy Pathing Framework

---
+++ Assets/Scripts/LaserCatcher.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserCatcher : MonoBehaviour
+{
+
+    /* Destroys anything that collides with it.
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(collision.gameObject);
+    }*/
+}
