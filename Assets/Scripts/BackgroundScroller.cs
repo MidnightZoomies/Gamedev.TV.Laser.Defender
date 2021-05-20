809eb6c24e8c2c42d6e2bf691cec709cb9979c73
MidnightZoomies <mnzoomies@gmail.com>
2021-05-20 12:57:47 -0700

    Added Starfield partical effects

--- Assets/Scripts/BackgroundScroller.cs
+++ Assets/Scripts/BackgroundScroller.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class BackgroundScroller : MonoBehaviour
 {
-    [SerializeField] float backgroundScrollSpeed = 0.5f;
+    [SerializeField] float backgroundScrollSpeedY = 0.03f;
+    //[SerializeField] float backgroundScrollSpeedX = 0.01f; Need a vertical and horizontal looping background
     Material myMaterial;
     Vector2 offset;
 
     void Start()
     {
         myMaterial = GetComponent<Renderer>().material;
-        offset = new Vector2(0f, backgroundScrollSpeed);
+        offset = new Vector2(0f, backgroundScrollSpeedY);
     }
 
     // Update is called once per frame
